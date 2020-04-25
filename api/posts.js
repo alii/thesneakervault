27366e6c484fc8ebd47474e70dd145ba1fcef491
Alistair Smith <alistairsmith01+git@gmail.com>
2020-04-25 11:02:03 +0100

    Vault

--- api/posts.js
+++ api/posts.js
@@ -1,16 +1,20 @@
-import { v4 as uuid } from 'uuid';
+import md5 from 'md5';
 
 const posts = [
   {
     title: 'Welcome to Sneaker Vault...',
     content:
-      'Hi everyone, my name is George – sneakerhead of 4 years and I have no children, because I am not of the legal age yet.',
+      "Hi everyone, my name is George – sneakerhead of 4 years and best friends with a programmer who you probably haven't heard of.",
     tags: ['Nike', 'Adidas', 'New', 'Hot'],
     image: 'https://i.imgur.com/8I0MuVQ.jpg',
     date: Date.now(),
-    id: uuid(),
   },
-];
+].map(post => {
+  return {
+    ...post,
+    id: md5(post.title),
+  };
+});
 
 export default (req, res) => {
   res.json({ success: true, data: posts });
